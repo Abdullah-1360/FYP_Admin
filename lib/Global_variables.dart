8617e7072c8d0ebb171ffda4cd9390cd81deca5e
Abdullah-1360 <124857916+Abdullah-1360@users.noreply.github.com>
2025-08-05 09:54:24 +0500

    Update Global_variables.dart

--- lib/Global_variables.dart
+++ lib/Global_variables.dart
@@ -1,7 +1,7 @@
 
 class globalvariables {
   // URLs
-  static const String _production = 'https://933tv38r-5000.inc1.devtunnels.ms/api';
+  static const String _production = 'https://x8n472ds-5000.inc1.devtunnels.ms/api';
   // Replace with your machine's LAN IP if testing on a real device
   static const String _local = 'http://192.168.100.187:5000/api';
   // Android emulator sees host machine at 10.0.2.2
